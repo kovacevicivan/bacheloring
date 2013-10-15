e07c892ad1ec625945a494f1c16de8ed160df42c
Ivan Kovacevic <ivan.kovacevic12@gmail.com>
2013-10-16 01:06:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bacheloring::Application.config.secret_key_base = '7c920e5952d7411a614fe02d4eb16789505116dc27b9445a82e8b7a993510838cc1f11e4c073bfbd76ec31cb71cf0f8d8ddd16ffe01dbb58e4e7136f585d4631'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bacheloring::Application.config.session_store :cookie_store, key: '_bacheloring_session'
